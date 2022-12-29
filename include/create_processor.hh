5f61b566ae34aff47060a3ea7b0f8e249261471c
KKSSainz <victor.trnka@gmail.com>
2022-12-29 12:43:37 +0100

    add info about created processor

--- include/create_processor.hh
+++ include/create_processor.hh
@@ -33,18 +33,22 @@ namespace bparser{
         switch (simd_size) {
             case 2:
             {
+                // std::cout << "** create processor with SSE4 --" << std::endl;
                 return create_processor_<Vec2d>(se, vector_size, simd_size, arena);
             } break;
             case 4:
             { 
+                // std::cout << "** create processor with AVX2 --" << std::endl;
                 return create_processor_<Vec4d>(se, vector_size, simd_size, arena);
             } break;
             case 8:
             {
+                // std::cout << "** create processor with AVX512 --" << std::endl;
                 return create_processor_<Vec8d>(se, vector_size, simd_size, arena);
             } break;
             default:
             {
+                // std::cout << "** create processor w/o vectorization --" << std::endl;
                 return create_processor_<double>(se, vector_size, 1, arena);
             } break;
         }
